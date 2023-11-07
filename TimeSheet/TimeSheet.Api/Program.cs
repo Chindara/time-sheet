c7d56b936850009cb990e0a67aa0025ba2ce85ef
Chinthaka Bandara <chinthakapb@gmail.com>
2023-11-07 22:04:10 +0530

    API project setup

---
+++ TimeSheet/TimeSheet.Api/Program.cs
@@ -0,0 +1,29 @@
+using Serilog;
+using TimeSheet.Application;
+using TimeSheet.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
